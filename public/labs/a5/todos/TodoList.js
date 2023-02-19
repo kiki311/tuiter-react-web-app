faf6ffb7bb078420b374e9bb4636be17f8d40a4f
kiki311 <kikiku2023@gmail.com>
2023-02-19 09:01:33 -0500

    changes

--- public/labs/a5/todos/TodoList.js
+++ public/labs/a5/todos/TodoList.js
@@ -1,38 +1,14 @@
-
-// const TodoList = () => {
-//     return(`
-//        <ul>
-//            <li>Buy milk</li>
-//            <li>Pickup the kids</li>
-//            <li>Walk the dog</li>
-//        </ul>
-//    `);
-// }
-// export default TodoList;
-
-// import TodoItem from "./TodoItem.js";
-// const TodoList = () => {
-//     return(`
-//        <ul>
-//            ${TodoItem('Buy milk')}
-//            ${TodoItem('Pickup the kids')}
-//            ${TodoItem('Walk the dog')}
-//        </ul>
-//    `);
-// }
-// export default TodoList;
-
 import TodoItem from "./TodoItem.js";
 import todos from "./todos.js";
 const TodoList = () => {
     return(`
-      <ul>
-         ${
+        <ul>
+            ${
         todos.map(todo => {
             return(TodoItem(todo));
         }).join('')
     }
-      </ul>
-   `);
+        </ul>
+    `);
 }
 export default TodoList;
