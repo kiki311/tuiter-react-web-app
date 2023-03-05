c9fae4b540fb4c23eb562584f132cbe7d4d17393
kiki311 <kikiku2023@gmail.com>
2023-03-05 15:38:52 -0500

    2.4

---
+++ src/labs/a6/index.js
@@ -0,0 +1,12 @@
+//
+// 2.4 Breaking out assignments into separate components
+
+
+function Assignment6() {
+    return (
+        <div>
+            <h1>Assignment 6</h1>
+        </div>
+    );
+}
+export default Assignment6;
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,10 +1,24 @@
 
 // 2.3 Implementing the Labs component
 
+// function Labs() {
+//     return(
+//         <div>
+//             <h1>Assignment 6</h1>
+//         </div>
+//     );
+// }
+// export default Labs;
+
+// 2.4 Breaking out assignments into separate components
+
+import Assignment6 from "./a6";
+
 function Labs() {
-    return(
+    return (
         <div>
-            <h1>Assignment 6</h1>
+
+            <Assignment6/>
         </div>
     );
 }
