05473e7e40fc7f9bb7c73b7f5befc45082a77196
kiki311 <kikiku2023@gmail.com>
2023-03-05 15:32:56 -0500

    2.2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // 2.2 Installing CSS libraries Bootstrap and Bootstrap Icons
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
