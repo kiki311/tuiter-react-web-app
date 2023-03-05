f58402008d73cd907c93c77ec92db968aa0b4aa3
kiki311 <kikiku2023@gmail.com>
2023-03-05 16:05:09 -0500

    2.9

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -7,20 +7,29 @@
 //     );
 // };
 // export default HelloWorld;
+//
+//  import {Link}
+//      from "react-router-dom";
+//
+// function HelloWorld() {
+//     return(
+//         <div>
+//             <Link to="/">Lab</Link> |
+//             <Link to="/hello">Hello</Link> |
+//             <Link to="/tuiter">Tuiter</Link>
+//             <h1>Hello World!</h1>
+//         </div>
+//     )
+// };
+// export default HelloWorld;
 
-
-import {Link}
-    from "react-router-dom";
-
+import Nav from "../nav";
 function HelloWorld() {
-    return(
+    return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
+            <Nav/>
             <h1>Hello World!</h1>
         </div>
-    )
-};
+    );
+}
 export default HelloWorld;
-
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -10,16 +10,27 @@
 // }
 // export default Tuiter
 
+//
+// import {Link}
+//     from "react-router-dom";
+//
+// function Tuiter() {
+//     return(
+//         <div>
+//             <Link to="/">Lab</Link> |
+//             <Link to="/hello">Hello</Link> |
+//             <Link to="/tuiter">Tuiter</Link>
+//             <h1>Tuiter</h1>
+//         </div>
+//     )
+// }
+// export default Tuiter
 
-import {Link}
-    from "react-router-dom";
-
+import Nav from "../nav";
 function Tuiter() {
-    return(
+    return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
+            <Nav/>
             <h1>Tuiter</h1>
         </div>
     )
