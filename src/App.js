44f2e56a78c161f3b32276cc1b8ce90f320bb1e5
kiki311 <kikiku2023@gmail.com>
2023-03-05 15:41:41 -0500

    2.5

--- src/App.js
+++ src/App.js
@@ -9,11 +9,27 @@
 // export default App;
 
 // 2.3 Implementing the Labs component
+// import Labs from "./labs";
+// function App() {
+//     return (
+//         <div className="container">
+//             <h1>Hello World!</h1>
+//             <Labs/>
+//         </div>
+//     );
+// }
+// export default App;
+
+// 2.5 Breaking out Hello World into a separate component
+
+
 import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+
 function App() {
     return (
         <div className="container">
-            <h1>Hello World!</h1>
+            <HelloWorld/>
             <Labs/>
         </div>
     );
---
+++ src/labs/a6/hello-world.js
@@ -0,0 +1,10 @@
+
+// 2.5 Breaking out Hello World into a separate component
+
+function HelloWorld() {
+    return(
+        <h1>Hello World!</h1>
+    );
+};
+export default HelloWorld;
+
