873d795edbe119434994a5ade397dd5eae43e3a3
kiki311 <kikiku2023@gmail.com>
2023-03-05 15:35:44 -0500

    2.3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
 
 // 2.1 Implementing Single Page Applications
+// function App() {
+//     return (
+//         <h1>Hello World!</h1>
+//     );
+// }
+// export default App;
+
+// 2.3 Implementing the Labs component
+import Labs from "./labs";
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <div className="container">
+            <h1>Hello World!</h1>
+            <Labs/>
+        </div>
     );
 }
 export default App;
---
+++ src/labs/index.js
@@ -0,0 +1,11 @@
+
+// 2.3 Implementing the Labs component
+
+function Labs() {
+    return(
+        <div>
+            <h1>Assignment 6</h1>
+        </div>
+    );
+}
+export default Labs;
