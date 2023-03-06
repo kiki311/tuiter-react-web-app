7012974d440cc3d662ff7dcb246767ee158c0a7b
kiki311 <kikiku2023@gmail.com>
2023-03-05 20:13:24 -0500

    tuiter

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -28,9 +28,9 @@ const ExploreComponent = () => {
                 </li>
             </ul>
             <div className="position-relative mb-2">
-                <img src="/images/starship.jpg" className="w-100"/>
+                <img src="/images/Starship-Mk1-Day.jpeg" className="w-100"/>
                 <h1 className="position-absolute wd-nudge-up text-white">
-                    SpaceX Starship</h1>
+                    </h1>
             </div>
             <PostSummaryList/>
         </>
